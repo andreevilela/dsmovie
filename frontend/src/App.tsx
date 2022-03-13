32a84ac9d9963bfba2ab78ee82e8449fedd558cd
andreevilela <andrevibegoogle@hotmail.com>
2022-03-13 02:25:28 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import {ReactComponent as GithubIcon} from './assets/img/github.svg' 
 
 function App() {
   return (
-    <h1>Página Principal</h1>
+    <header>
+    <nav className="container">
+        <div className="dsmovie-nav-content">
+            <h1>DSMovie</h1>
+            <a href="https://github.com/andreevilela" target="_blank" rel="noreferrer">
+                <div className="dsmovie-contact-container">
+                    <GithubIcon />
+                    <p className="dsmovie-contact-link">/andreevilela</p>
+                </div>
+            </a>
+        </div>
+    </nav>
+</header>
   );
 }
 
